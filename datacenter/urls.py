86b45e9bd22b75ebe9c325b10b7313708962d783
skynettw <skynet.tw@gmail.com>
2023-04-22 15:55:53 +0800

    first commit

---
+++ datacenter/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from mysite import views  #去mysite資料夾中匯入views.py這個檔案
+
+urlpatterns = [
+    path('', views.index),
+    path('lucky/', views.lucky),
+    path('admin/', admin.site.urls),
+]
---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render  # (渲染: 1.來自於瀏覽器的請求 2.HTML範本 3.要送過去的變數資料)
+import random
+
+def index(request):
+    return render(request, "index.html", locals())
+
+def lucky(request):
+    name = random.choice(["王小明", "林小華", "林書豪"])
+    lucky = random.randint(1, 49)
+    return render(request, "lucky.html", locals())  # locals()會打包所有區域內的變數
