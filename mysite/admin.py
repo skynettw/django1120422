68660213390947511e478bee586a680e5b8ab431
skynettw <skynet.tw@gmail.com>
2023-04-23 15:36:26 +0800

    add bodyinfo to admin.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from mysite.models import News
+from mysite.models import News, BodyInfo
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'source', 'pdate')
+
+@admin.register(BodyInfo)
+class BodyInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'height', 'weight')
